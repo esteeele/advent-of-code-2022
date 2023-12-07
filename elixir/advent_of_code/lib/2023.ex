ad6f2c4fc612676a5749636856775a14e0b61e43
Ed Steele <ed.steele@starlingbank.com>
2023-12-07 09:12:43 +0000

    day 6

--- elixir/advent_of_code/lib/2023.ex
+++ elixir/advent_of_code/lib/2023.ex
@@ -1,4 +1,6 @@
 defmodule Advent2023 do
+  alias Advent2023.SeedMaps
+
   def solve(red, green, blue) do
     instructions =
       File.read!("./lib/input.txt")
@@ -60,16 +62,19 @@ defmodule Advent2023 do
   end
 
   def day3Part2 do
+    inputLines = parseInput()
+
     cardScoreLookup =
-      parseInput()
+      inputLines
       |> Enum.reduce(%{}, fn line, cardMap ->
         [card, numbers] = splitAndTrim(line, ":")
         winners = mapCardLineToWinningNumbers(numbers)
+        # i'm not sure why this did what I expected it to ...
         [_card, number] = String.split(card)
         Map.put(cardMap, number, length(winners))
       end)
 
-    [head | tail] = parseInput()
+    [head | tail] = inputLines
     iterateCardList(head, tail, length(tail) + 1, cardScoreLookup)
   end
 
@@ -121,5 +126,163 @@ defmodule Advent2023 do
   defp parseInput() do
     File.read!("./lib/input.txt")
     |> String.split("\n", trim: true)
+  end
+
+  defp convert_to_int(text) do
+    Integer.parse(text) |> elem(0)
+  end
+
+  # day 5
+  defmodule SeedMaps do
+    defstruct source: "",
+              destination: "",
+              sourceStart: 0,
+              destStart: 0,
+              offset: 0,
+              transformation: 0
+  end
+
+  defmodule Range do
+    defstruct range_start: nil, range_end: nil
+  end
+
+  def day5P1 do
+    value = %SeedMaps{}
+    inputLines = parseInput()
+    [seeds | tail] = inputLines
+    IO.inspect(seeds)
+
+    # process seeds
+    all_mappings =
+      Enum.reduce(tail, %{}, fn line, map ->
+        cond do
+          String.contains?(line, "-to-") ->
+            [source, "to", destination] =
+              String.split(String.replace(line, " map:", ""), "-", trim: true)
+
+            map = Map.put(map, "current_source", source)
+            Map.put(map, "current_dest", destination)
+
+          Regex.match?(~r/\d+/, line) ->
+            [dest_start, source_start, range_size] =
+              String.split(line, " ", trim: true)
+              |> Enum.map(fn elem -> Integer.parse(elem) |> elem(0) end)
+
+            # add to a list for the source map
+            mapping = %SeedMaps{
+              source: Map.get(map, "current_source"),
+              destination: Map.get(map, "current_dest"),
+              sourceStart: source_start,
+              destStart: dest_start,
+              offset: range_size,
+              transformation: dest_start - source_start
+            }
+
+            Map.update(map, Map.get(map, "current_source"), [mapping], fn existing_list ->
+              [mapping | existing_list]
+            end)
+
+          true ->
+            map
+        end
+      end)
+    IO.inspect(all_mappings)
+
+    IO.inspect(tl(splitAndTrim(seeds, ":")))
+
+    least_location =
+      tl(splitAndTrim(seeds, ":"))
+      |> hd()
+      |> splitAndTrim(" ")
+      |> Enum.map(fn value -> convert_to_int(value) end)
+      |> Enum.map(fn seedValue ->
+        down_the_seed_hole(%Range{range_start: seedValue, range_end: seedValue}, all_mappings, "seed")
+      end)
+      |> Enum.min()
+
+    # reprocess the mappings map working backwards finding for each mapping zone finding
+    # the least offset
+
+    seeds =
+      tl(splitAndTrim(seeds, ":"))
+      |> hd()
+      |> splitAndTrim(" ")
+      |> Enum.map(fn value -> convert_to_int(value) end)
+      |> Enum.chunk_every(2, 2)
+      |> Enum.reduce(%{}, fn [start, range], lookup_acc ->
+        lookup_acc
+      end)
+
+    """
+    Work from back to front building up a ds that points a range to the min
+    value - if something is outside this range it is self
+    """
+
+    # seeds |> Enum.min_by(fn {_k, v} -> v end)
+    # IO.inspect(seeds)
+    # build maps
+    least_location
+  end
+
+  defp down_the_seed_hole(range, mappings, source) do
+    mapping_list = Map.get(mappings, source)
+    range_start = range.range_start
+    range_end = range.range_end
+    IO.inspect(length(mapping_list))
+
+    filtered_map =
+      Enum.filter(mapping_list, fn seed_map ->
+        # here replace this with range overlap vs individual
+        # so like split out the range based on what overlaps
+        # need to build a map of which ranges point to which offsets
+        # for a range, find the number of ways it could be mapped then find the min of these
+        # and also store the values in case the same range gets hit a second time?
+
+        #completely contained in another range, what happens if not?
+        range_start >= seed_map.sourceStart and range_end  <= seed_map.sourceStart + seed_map.offset
+      end)
+
+    next_dest = hd(mapping_list).destination
+    possible_transformations = case filtered_map do
+      [] -> [0]
+      _ -> Enum.map(filtered_map, fn map -> map.transformation end)
+    end
+    Enum.map(possible_transformations, fn transformation ->
+      next_input = range_start + transformation
+      if next_dest == "location" do
+        next_input
+      else
+        down_the_seed_hole(%Range{range_start: next_input, range_end: next_input}, mappings, next_dest)
+      end
+    end)
+    |> Enum.min()
+  end
+
+  def day6P1 do
+    times_and_distances = parseInput()
+      |> Enum.map(fn line -> splitAndTrim(line, ":") |> tl() end)
+      |> Enum.map(fn numbers ->
+        splitAndTrim(hd(numbers), " ")
+        |> Enum.map(fn number_str -> convert_to_int(number_str) end)
+      end)
+      |> Enum.zip()
+
+    [time, distance] = parseInput()
+      |> Enum.map(fn line -> splitAndTrim(line, ":") |> tl() end)
+      |> Enum.map(fn numbers ->
+        convert_to_int(String.replace(hd(numbers), " ", ""))
+      end)
+
+    #part 2
+    times_and_distances = [{time, distance}]
+
+    Enum.map(times_and_distances, fn {race_time, distance} ->
+      # y = race_time * x - x^2 and y = distance
+      quadratic = abs(-race_time + (:math.sqrt(race_time * race_time - 4 * -1 * -distance))) / 2
+      range_start = ceil(quadratic + 0.01) # ensures we round to the nearest int + 1 (lack of Elixir knowledge...)
+      range_end = race_time - floor(quadratic)
+      range_end - range_start
+    end)
+    |> Enum.reduce(1, fn possible_ways, acc -> acc * possible_ways end)
   end
 end
